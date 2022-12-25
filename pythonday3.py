3fa2c7feca6ee8393b9ff316c28b86dbd7d656a7
pedro Vinicius <singed161@gmail.com>
2022-12-25 20:48:38 -0300

    day3 py

---
+++ pythonday3.py
@@ -0,0 +1,15 @@
+print ("Welcome to the Rollercoaster!")
+height = int(input("what is  your height ib CM? "))
+if height > 120:
+    print ("Good You can ride the rollercoaster !!!")
+else:
+    print("how bad you can't ride the roller coaster")
+
+#operators
+
+#< greater than
+#< less than
+#>= greater than or equal to
+#<= less than or equal to
+#== equal to
+#!= not equal to
