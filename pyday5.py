9f9d2b0ed7613e38074a63d5a180dc4376222196
pedro Vinicius <singed161@gmail.com>
2022-12-29 22:17:03 -0300

    moedinha

--- pyday5.py
+++ pyday5.py
@@ -3,3 +3,14 @@
 #Os modulos sao codigos separados Reponsaveis pela funcionalidade de de um grande codigo
 #é como criar um carro cada Modulo é como se fosse uma "parte do carro" no processo de criaçao
 # para utilizar os modulos é so necessario dar um import em cada page
+
+#
+
+import random
+
+random_side = random.randint(0,1)
+
+if random_side == 1:
+    print("heads")
+else:
+    print("tails")
