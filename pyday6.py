45f1f8ea5c24780aafb4ab74bd53d33a251fab6b
pedro Vinicius <singed161@gmail.com>
2022-12-29 23:01:22 -0300

    Update pyday6.py

--- pyday6.py
+++ pyday6.py
@@ -7,3 +7,15 @@
 #O que realmente importa é a sintaxe. Sempre Parenteses Retos e no meio os artigos separados
 #por uma virgula
 # EX: fruits = [ "melon","banana", "cherry"...]
+# print(fruits[0])
+# melon
+
+# A ordem na qual so dados são armazenados na lista sao de grande importancia
+#pois a maneira que os dados são armazenados influenciam quando so dados sao acessados
+#no dado acima o "melon" vai ser sempre o primeiro dado da lista, o mesmo vale para a variavel
+#ORDEM é importante no codigo.
+
+#posso utilizar indice negaticovos para ter acesso aos dados finais de uma list
+# ex fruits = [ "melon","banana", "cherry"...]
+# print(fruits[-1])
+# cherry
