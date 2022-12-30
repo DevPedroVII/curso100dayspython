837e242d00589cb1ed81a5333aa628bc98ce8958
pedro Vinicius <singed161@gmail.com>
2022-12-29 22:33:55 -0300

    Update pyday6.py

--- pyday6.py
+++ pyday6.py
@@ -1,2 +1,6 @@
 #Python List / DATA STRUCTURE
 #é uma forma de armazanar dados
+
+#LIST
+#É um conjunto de parenteses Rectos com muitos artigos armazenados
+# EX: fruits = [ "melon","banana", "cherry"...]
