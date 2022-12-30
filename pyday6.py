d5271db732afbc74432c5b2382f1e390857b4076
pedro Vinicius <singed161@gmail.com>
2022-12-29 22:40:07 -0300

    Update pyday6.py

--- pyday6.py
+++ pyday6.py
@@ -4,4 +4,6 @@
 #LIST
 #É um conjunto de parenteses Rectos com muitos artigos armazenados,
 #pode ser X tipos de dados e eles podendo ser mistos (Strings, Numbers,Boleans ETCs)
+#O que realmente importa é a sintaxe. Sempre Parenteses Retos e no meio os artigos separados
+#por uma virgula
 # EX: fruits = [ "melon","banana", "cherry"...]
