c53d7782b10d2de5d4866ac050ed649823ba275a
pedro Vinicius <singed161@gmail.com>
2022-12-29 22:09:46 -0300

    Update pyday5.py

---
+++ pyday5.py
@@ -0,0 +1,5 @@
+#Modules in Python
+
+#Os modulos sao codigos separados Reponsaveis pela funcionalidade de de um grande codigo
+#é como criar um carro cada Modulo é como se fosse uma "parte do carro" no processo de criaçao
+# para utilizar os modulos é so necessario dar um import em cada page
