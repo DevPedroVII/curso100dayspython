b52100925a59e64b818b577c15097cf97c09494c
pedro Vinicius <singed161@gmail.com>
2022-12-29 22:37:00 -0300

    Update pyday6.py

--- pyday6.py
+++ pyday6.py
@@ -2,5 +2,6 @@
 #é uma forma de armazanar dados
 
 #LIST
-#É um conjunto de parenteses Rectos com muitos artigos armazenados
+#É um conjunto de parenteses Rectos com muitos artigos armazenados,
+#pode ser X tipos de dados e eles podendo ser mistos (Strings, Numbers,Boleans ETCs)
 # EX: fruits = [ "melon","banana", "cherry"...]
