3e52ed02a531c0a2f61359e7ec9e2e4fa526e015
pedro Vinicius <singed161@gmail.com>
2022-12-30 22:41:46 -0300

    py7

---
+++ py7.py
@@ -0,0 +1,13 @@
+#complex()
+#O método complex() retorna um número complexo quando partes reais
+#e imaginárias são fornecidas ou converte uma string em um número complexo.
+
+#A sintaxe de complex()é:
+#complex([real[, imagem]])
+
+#Em geral, o complex()método recebe dois parâmetros:
+#real - parte real. Serealfor omitido, o padrão é 0.
+#imagem - parte imaginária. Seimagemfor omitido, o padrão é 0.
+#Se o primeiro parâmetro passado para este método for uma string,
+#ela será interpretada como um número complexo.
+#Neste caso, o segundo parâmetro não deve ser passado.
