b85defc70eb1e41ce59205b16ad86c43af79c0e7
pedro Vinicius <singed161@gmail.com>
2023-01-05 19:15:47 -0300

    Update pyday12.py

---
+++ pyday12.py
@@ -0,0 +1,12 @@
+#Python Keywords
+
+#A inpalavra-chave tem dois propósitos:
+# É usada para verificar se um valor está presente em uma sequência (lista, intervalo, string etc.).
+# Também é usada para percorrer uma sequência em um forloop:
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
+if "banana" in fruits:
+  print("yes")
