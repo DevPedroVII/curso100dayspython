395a0b282ab8664627262c1caafb3cd19cba07a0
pedro Vinicius <singed161@gmail.com>
2023-01-05 20:00:57 -0300

    Update pyday12.py

--- pyday12.py
+++ pyday12.py
@@ -59,3 +59,16 @@ for fruta in ['Abacaxi', 'Morango', 'Uva']:
 #    if <condicao_verdadeira>:
 #         <outras_instrucoes>
 #         break
+
+#exemplo pratico
+
+pessoas = [({'nome': 'João', 'cidade': 'Belo Horizonte'}),
+                     ({'nome': 'Maria', 'cidade': 'São Paulo'}),
+                     ({'nome': 'Pedro', 'cidade': 'Curitiba'})]
+contador = 0
+for pessoa in pessoas:
+    contador += 1
+    print(contador)
+    if pessoa['nome'] == 'Maria':
+        print(pessoa['nome'], "mora em", pessoa['cidade'])
+        break
