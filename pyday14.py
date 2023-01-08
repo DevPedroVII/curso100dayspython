852816ad147f72a540859f2472d0180e1943d27c
pedro Vinicius <singed161@gmail.com>
2023-01-07 21:53:26 -0300

    Update pyday14.py

--- pyday14.py
+++ pyday14.py
@@ -6,3 +6,8 @@
 
 #pop	nenhum	Remove e returno o último item
 #pop	posição	Remove e retorna o item da posição.
+
+##Pertinência em uma Lista
+
+#in e not in são operadores booleanos ou lógicos que testam a pertinência (membership)
+#em uma sequência. Já usamos esses operadores com strings e eles também funcionam aqui.
