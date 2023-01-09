89136ba90ce9fac26afaca57d28957f768b62274
pedro Vinicius <singed161@gmail.com>
2023-01-08 21:17:55 -0300

    Update pyday15.py

--- pyday15.py
+++ pyday15.py
@@ -1,3 +1,5 @@
 lista_de_numeros = [1, 2 , 3 , 4 , 5 , 6 , 7, 8, 9, 10]
 Resposta = int(input("Qual é o numero ? "))
 
+for numero in lista_de_numeros:
+    print(f"{numero} x {Resposta} = {numero * Resposta } " )
