af9ace72ef185be3b79e1f3b165ab4796bcc2ee1
pedro Vinicius <singed161@gmail.com>
2023-01-08 21:20:34 -0300

    Update pyday15.py

--- pyday15.py
+++ pyday15.py
@@ -2,4 +2,21 @@ lista_de_numeros = [1, 2 , 3 , 4 , 5 , 6 , 7, 8, 9, 10]
 Resposta = int(input("Qual é o numero ? "))
 
 for numero in lista_de_numeros:
-    print(f"{numero} x {Resposta} = {numero * Resposta } " )
+    print(f"{numero} x {Resposta} = {numero * Resposta }/n") #Python utiliza o caractere \n para quebra de linhas.
+
+
+lista_de_numeros = [1, 2 , 3 , 4 , 5 , 6 , 7, 8, 9, 10]
+Resposta = int(input("Qual é o numero ? "))
+
+for numero in lista_de_numeros:
+    print(f"{numero} + {Resposta} = {numero + Resposta }/n")
+
+lista_de_numeros = [1, 2 , 3 , 4 , 5 , 6 , 7, 8, 9, 10]
+Resposta = int(input("Qual é o numero ? "))
+
+
+
+
+
+
+
