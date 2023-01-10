3011d9f92fe5f660d29d9f9755580aaef1a0329a
pedro Vinicius <singed161@gmail.com>
2023-01-09 21:10:34 -0300

    Update pyday16.py

--- pyday16.py
+++ pyday16.py
@@ -11,3 +11,7 @@
 #funções são blocos de código que realizam
 # determinadas tarefas que normalmente precisam ser executadas
 # diversas vezes dentro de uma aplicação.
+
+#A sintaxe de uma função é definida por três partes
+# : nome, parâmetros e corpo,
+# o qual agrupa uma sequência de linhas que representa algum comportamento.
