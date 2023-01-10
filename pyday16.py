6b86086fbb24ed6166d72bd27fd3b255290f4b0c
pedro Vinicius <singed161@gmail.com>
2023-01-09 21:08:37 -0300

    Create pyday16.py

---
+++ pyday16.py
@@ -0,0 +1,7 @@
+#Function
+
+#Funções são um recurso poderoso das linguagens de programação.
+# Ao desenvolver uma aplicação utilizamos elas a todo momento,
+# quer sejam as que nós mesmos escrevemos para as rotinas especificas ou
+# aquelas já disponibilizadas pela linguagem de programação escolhida,
+# suas bibliotecas e frameworks.
