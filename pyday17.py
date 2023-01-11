16e7c82b7d1078cde764dfbcc5d3d73913746648
pedro Vinicius <singed161@gmail.com>
2023-01-10 21:58:00 -0300

    Update pyday17.py

--- pyday17.py
+++ pyday17.py
@@ -3,3 +3,11 @@
 #O Python nos disponibiliza um tipo que preenche muito melhor as características que procurávamos - as tuplas. Tuplas podem ser consideradas similares às listas, mas suas diferenças são cruciais.
 #Quanto à sintaxe, a tupla se diferencia por substituir os colchetes ([]) das listas por parênteses (()):
 
+caelum_coordenadas = (-23.588254, -46.632477)
+print(type(caelum_coordenadas))
+
+#Em primeiro lugar,
+# a diferença mais explícita pela própria comunidade Python é que tuplas são imutáveis.
+# Isso significa que não podemos alterar um mesmo objeto tupla,
+# ou seja, mudar uma de suas referências internas (seus valores),
+# nem adicionar ou remover elemento algum
