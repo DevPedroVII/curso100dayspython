7e60f610d6a9d3d916abd60596b74d4573b8498d
pedro Vinicius <singed161@gmail.com>
2023-01-10 21:55:08 -0300

    Update pyday17.py

---
+++ pyday17.py
@@ -0,0 +1,5 @@
+# TUPLAS
+
+#O Python nos disponibiliza um tipo que preenche muito melhor as características que procurávamos - as tuplas. Tuplas podem ser consideradas similares às listas, mas suas diferenças são cruciais.
+Quanto à sintaxe, a tupla se diferencia por substituir os colchetes ([]) das listas por parênteses (()):
+
