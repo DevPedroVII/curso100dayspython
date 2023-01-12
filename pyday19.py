18ee9256b0ba6b494a0fc3c0ec2b684bcf3d2a83
pedro Vinicius <singed161@gmail.com>
2023-01-12 20:12:41 -0300

    Update pyday19.py

--- pyday19.py
+++ pyday19.py
@@ -1,3 +1,10 @@
 #Método Python Set union ()
 
 #Retorna um conjunto que contém todos os itens de ambos os conjuntos, as duplicatas são excluídas
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.union(y)
+
+print(z)
