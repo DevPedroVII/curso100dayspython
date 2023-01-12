5e72db8affd570495392658cdd03ef7a82930a1b
pedro Vinicius <singed161@gmail.com>
2023-01-11 21:55:53 -0300

    Update pyday18.py

--- pyday18.py
+++ pyday18.py
@@ -1,3 +1,12 @@
 #Sets – Conjuntos em Python
 
-Sets – Conjuntos em Python
+#Armazena apenas itens não-duplicados (únicos)
+#Suporta operações matemáticas sobre conjuntos (união, intersecção, etc)
+#Não é possível modificar os itens existentes, mas podemos adicionar novos elementos (sets são mutáveis).
+#Suporta elementos de qualquer tipo, não-ordenados e não-indexados.
+#Podem conter apenas objetos imutáveis, como strings, ints, floats e tuplas de objetos também imutáveis.
+#São escritos com chaves.
+#Os sets são iteráveis (podemos acessar seus elementos um a um),
+# e não são considerados sequências como as tuplas e as listas.
+# Eles também não suportam indexação e slicing. Assim,
+# os itens de um set são retornados em uma ordem diferente cada vez que acessamos o conjunto todo.
