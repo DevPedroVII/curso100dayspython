c0f87c9ff86bc3f89ed9c1a939cd41a763b84003
pedro Vinicius <singed161@gmail.com>
2023-01-13 20:33:06 -0300

    Update pyday20.py

---
+++ pyday20.py
@@ -0,0 +1,9 @@
+#Dicionarios
+
+#Os dicionários são coleções de itens e seus elementos são armazenados de forma não ordenada.
+
+#Exemplo de sua sintaxe:
+
+#dicio = {'chave': 'valor'}
+
+#print(type(dicio))
