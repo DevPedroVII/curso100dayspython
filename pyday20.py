aef92a9cce43423e8773ebb607cae781581ac373
pedro Vinicius <singed161@gmail.com>
2023-01-13 20:36:45 -0300

    Update pyday20.py

--- pyday20.py
+++ pyday20.py
@@ -17,14 +17,20 @@
 #dicio_2 = {'primeiro': 1, 'segundo':  2, 'terceiro': 3}
 #Também podemos utilizar a função dict do próprio Python (built-in function), passando as chaves e valores como parâmetros:
 
-#1
 #dicio = dict(primeiro=1, segundo=2, terceiro=3)
 #Utilizando a função zip para concatenar a chave:valor em um elemento do objeto dict:
 
-#1
 #dicio_3 = dict(zip(['primeiro', 'segundo', 'terceiro'], [1, 2, 3]))
 #Utilizando uma lista de tuplas com itens simbolizando chave e valor em um objeto dict:
 
-#1
-#icio_4 = dict([('primeiro', 1), ('segundo', 2), ('terceiro', 3)])
 
+#dicio_4 = dict([('primeiro', 1), ('segundo', 2), ('terceiro', 3)])
+
+#Com Dict Comprehensions!
+
+
+#dicio_6 = {name: idx + 1 for idx, name in enumerate(('primeiro', 'segundo', 'terceiro'))}
+#Por mais estranho que pareça, podemos tentar transformar uma variável do tipo dicionário em dicionário.
+
+
+#dicio_5 = dict({'primeiro': 1, 'segundo':  2, 'terceiro': 3})
