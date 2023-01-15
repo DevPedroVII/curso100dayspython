a22a70cde8c08a79226cb6e12de1af85037f9003
pedro Vinicius <singed161@gmail.com>
2023-01-14 22:07:48 -0300

    Update pyday21.py

--- pyday21.py
+++ pyday21.py
@@ -1,1 +1,7 @@
 #selenium
+
+#As ligações do Selenium Python fornecem uma API simples para escrever testes funcionais/de aceitação usando o Selenium
+# WebDriver. Através da Selenium Python API você pode acessar todas as funcionalidades do Selenium WebDriver de forma intuitiva.
+#As ligações do Selenium Python fornecem uma API conveniente para acessar Selenium WebDrivers como Firefox,
+# Ie, Chrome, Remote etc. As versões atuais do Python suportadas são 3.5 e superiores.
+#Esta documentação explica a API Selenium 2 WebDriver. Selenium 1 / Selenium RC API não é abordado aqui.
