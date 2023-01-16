a8ba6e84fb174ae2c1a6e758893d701241af8cee
pedro Vinicius <singed161@gmail.com>
2023-01-15 21:10:54 -0300

    Update pyday22.py

--- pyday22/pyday22.py
+++ pyday22/pyday22.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r"./chromediver.exe")
-driver.get("https://www.youtube.com/watch?v=Pax0jiAcTWs&list=PLOQgLBuj2-3LqnMYKZZgzeC7CKCPF375B&index=7&ab_channel=EduardoMendes")
+driver = webdriver.Chrome(executable_path=r'./chromediver.exe')
+driver.get("https://www.youtube.com/watch?v=YBS8rJvxnKo")
 
