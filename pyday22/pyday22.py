6ab0b31ad6d0833f449649ffdb3cf2abb8ff5df9
pedro Vinicius <singed161@gmail.com>
2023-01-15 21:19:22 -0300

    HTML
    body

--- pyday22/pyday22.py
+++ pyday22/pyday22.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path=r'./chromediver.exe')
 driver.get("https://www.youtube.com/watch?v=YBS8rJvxnKo")
 
+
+#HTML (HyperText Markup Language)
+
+# Esquelto do browser.
