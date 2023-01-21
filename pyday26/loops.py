a8e0c817c68dd0d0b8be602eacef91ea0e807dc5
pedro Vinicius <singed161@gmail.com>
2023-01-20 22:00:38 -0300

    Update loops.py

--- pyday26/loops.py
+++ pyday26/loops.py
@@ -1,3 +1,7 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
+    elif number % 3 == 0:
+    print("Fizz")
+    elif number % 5 == 0:
+    print("Buzz")
