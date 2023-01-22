1efd983540e9a28de07e6805fab09549c1ab513b
pedro Vinicius <singed161@gmail.com>
2023-01-21 22:46:20 -0300

    Update Estruturas de dados.py

--- pyday27/Estruturas de dados.py
+++ pyday27/Estruturas de dados.py
@@ -7,3 +7,12 @@
 #Prolonga a lista,
 #adicionando no fim todos os elementos do argumento iterable passado como parâmetro.
 #Equivalente a a[len(a):] = iterable.
+
+#list.insert(i, x)
+#Insere um item em uma dada posição.
+#O primeiro argumento é o índice do elemento antes do qual será feita a inserção,
+#assim a.insert(0, x) insere um elemento na frente da lista e a.insert(len(a), x) e equivale a a.append(x).
+
+#list.remove(x)
+#Remove o primeiro item encontrado na lista cujo valor é igual a x.
+# Se não existir valor igual, uma exceção ValueError é levantada.
