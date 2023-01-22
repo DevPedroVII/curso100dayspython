4cdc97cd94c678785c2e764b2581152b3ceb53f5
pedro Vinicius <singed161@gmail.com>
2023-01-22 18:42:12 -0300

    Update MIMDE papai.py

--- pyday29/MIMDE papai.py
+++ pyday29/MIMDE papai.py
@@ -4,4 +4,7 @@ from discord.ext import commands
 
 bot = commands.Bot("!")
 
+async def on_ready():
+    print("MIMDE PAPAI")
+
 bot.run("MTA2NjgzMTM1NzUyOTA1MTI2OQ.GWGVDE.rLXF190LxzvQyFXMNqlfpgHMh3OVtbrJ4Qb9E8")
