b08f6688a57d5a1779f5c9bb344d70d1431937dd
pedro Vinicius <singed161@gmail.com>
2023-01-21 22:45:29 -0300

    Update Estruturas de dados.py

--- pyday27/Estruturas de dados.py
+++ pyday27/Estruturas de dados.py
@@ -1,3 +1,9 @@
 #Estruturas de dados
-list.append(x)
+
+#list.append(x)
 #Adiciona um item ao fim da lista. Equivalente a a[len(a):] = [x].
+
+#list.extend(iterable)
+#Prolonga a lista,
+#adicionando no fim todos os elementos do argumento iterable passado como parâmetro.
+#Equivalente a a[len(a):] = iterable.
