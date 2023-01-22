3966a7fccdb8da0af0397cf754ff2dd2b817154b
pedro Vinicius <singed161@gmail.com>
2023-01-21 22:49:09 -0300

    Update Estruturas de dados.py

--- pyday27/Estruturas de dados.py
+++ pyday27/Estruturas de dados.py
@@ -35,3 +35,23 @@
 #Devolve uma cópia rasa da lista. Equivalente a a[:].
 
 #example
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+
+fruits.count('tangerine')
+
+fruits.index('banana')
+
+fruits.index('banana', 4)
+
+fruits.reverse()
+fruits
+
+fruits.append('grape')
+fruits
+
+fruits.sort()
+fruits
+
+fruits.pop()
