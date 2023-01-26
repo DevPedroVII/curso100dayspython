71ee3e2bdc858823f8c89e641602059f4244c3da
pedro Vinicius <singed161@gmail.com>
2023-01-25 21:01:45 -0300

    Update randomkeygen.py

--- pyday30/randomkeygen.py
+++ pyday30/randomkeygen.py
@@ -3,10 +3,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+
 
 #Eazy Level
 # password = ""
