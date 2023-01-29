5429640c3fb1da8ee044ea6d641d6be125ee0cc1
pedro Vinicius <singed161@gmail.com>
2023-01-28 22:18:39 -0300

    Update Html.py

--- PYDAY32/Html.py
+++ PYDAY32/Html.py
@@ -9,4 +9,5 @@
  # SA marcação HTML inclui "elementos" especiais como <head>,
  # <title>, <body>, <header>, <footer>, <article>, <section>,
  # <p>, <div>, <span>, <img>, <aside>, <audio>, <canvas>,
- # <datalist>, <details>, <embed>, <nav>, <output>, <progress>, <video>, <ul>, <ol>e <li>muitos outros.
+ # <datalist>, <details>, <embed>, <nav>, <output>, <progress>,
+ # <video>, <ul>, <ol>e <li>muitos outros.
