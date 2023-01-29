7f1c0e290d77b590cb0128c6737d717d236d0a2f
pedro Vinicius <singed161@gmail.com>
2023-01-29 20:47:56 -0300

    Update pysite.py

---
+++ pyday31/pysite.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# route -> hashtagtreinamentos.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
