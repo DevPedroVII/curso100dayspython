42039334d74143ad62970a9abd16f4677b0682a1
pedro Vinicius <singed161@gmail.com>
2023-01-28 22:17:39 -0300

    Update Html.py

--- PYDAY32/Html.py
+++ PYDAY32/Html.py
@@ -1,4 +1,5 @@
  #Html
 
  # HTML (HyperText Markup Language)
- # é o bloco de construção mais básico da Web. Ele define o significado e a estrutura do conteúdo da web.
+ # é o bloco de construção mais básico da Web.
+ # Ele define o significado e a estrutura do conteúdo da web.
