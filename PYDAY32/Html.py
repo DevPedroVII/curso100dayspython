c9f8c0c54eba9a514a8f963e563475b4bf42fe48
pedro Vinicius <singed161@gmail.com>
2023-01-28 22:18:28 -0300

    Update Html.py

--- PYDAY32/Html.py
+++ PYDAY32/Html.py
@@ -3,3 +3,10 @@
  # HTML (HyperText Markup Language)
  # é o bloco de construção mais básico da Web.
  # Ele define o significado e a estrutura do conteúdo da web.
+
+ #HTML usa "marcação" para anotar texto,
+ # imagens e outros conteúdos para exibição em um navegador da Web.
+ # SA marcação HTML inclui "elementos" especiais como <head>,
+ # <title>, <body>, <header>, <footer>, <article>, <section>,
+ # <p>, <div>, <span>, <img>, <aside>, <audio>, <canvas>,
+ # <datalist>, <details>, <embed>, <nav>, <output>, <progress>, <video>, <ul>, <ol>e <li>muitos outros.
