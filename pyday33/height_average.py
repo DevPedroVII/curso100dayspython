3fda87346da43ef79eaf1d65878b4e4405cb9ff6
pedro Vinicius <singed161@gmail.com>
2023-01-30 20:45:03 -0300

    Update height_average.py

--- pyday33/height_average.py
+++ pyday33/height_average.py
@@ -1,3 +1,9 @@
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"total height = {total_height}")
+
