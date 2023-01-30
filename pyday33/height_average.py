9a4c9fe6fc3b39672875ee8123c5b46b9c9fab6f
pedro Vinicius <singed161@gmail.com>
2023-01-30 20:45:43 -0300

    Update height_average.py

--- pyday33/height_average.py
+++ pyday33/height_average.py
@@ -11,3 +11,6 @@ number_of_students = 0
 for student in student_heights:
   number_of_students += 1
 print(f"number of students = {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+print(average_height)
