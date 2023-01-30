94e8aa551f1aab2b496c3e97fdb9853ecf65c8da
pedro Vinicius <singed161@gmail.com>
2023-01-30 20:45:26 -0300

    Update height_average.py

--- pyday33/height_average.py
+++ pyday33/height_average.py
@@ -7,3 +7,7 @@ for height in student_heights:
   total_height += height
 print(f"total height = {total_height}")
 
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
