73d1bf238b5c5b981a52a05ef58b36c2064b37a4
pedro Vinicius <singed161@gmail.com>
2023-01-31 19:23:05 -0300

    Update media.py

--- pyday34/media.py
+++ pyday34/media.py
@@ -1,3 +1,6 @@
+#Instructions
+#You are going to write a program that calculates the highest score from a List of scores.S
+
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
