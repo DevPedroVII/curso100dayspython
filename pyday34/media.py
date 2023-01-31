ef768790e63672d49d674480e9f49aeab8449dc7
pedro Vinicius <singed161@gmail.com>
2023-01-31 19:21:55 -0300

    Update media.py

--- pyday34/media.py
+++ pyday34/media.py
@@ -2,3 +2,7 @@ student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
