4146b4e8f2ab0d15d75b68bee2c5e8bd8144bd37
pedro Vinicius <singed161@gmail.com>
2023-02-01 19:23:36 -0300

    Update pyday35.py

--- pyday35.py
+++ pyday35.py
@@ -7,3 +7,9 @@ print(even_sum)
 #or
 
 # alternative_sum = 0
+# for number in range(1, 101):
+#   if number % 2 == 0:
+#     # print(number)
+#     alternative_sum += number
+# print(alternative_sum)
+
