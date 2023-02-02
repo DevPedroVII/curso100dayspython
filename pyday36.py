6747078a5810a50f4a8e2cb3a957685e9d4b977a
pedro Vinicius <singed161@gmail.com>
2023-02-02 19:33:33 -0300

    Update pyday36.py

--- pyday36.py
+++ pyday36.py
@@ -3,3 +3,4 @@ from selenium.webdriver import Firefox
 url =' https://curso-pthon-netlify.app/aula_03.html'
 
 navegador = Firefox()
+navegador.get(url) - # poderia ser :navegador.get( https://curso-pthon-netlify.app/aula_03.html)
