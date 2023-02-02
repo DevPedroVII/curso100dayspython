b21f359e14613e0b6795f50f48e7e3c6fe1ec3ba
pedro Vinicius <singed161@gmail.com>
2023-02-02 19:34:14 -0300

    Update pyday36.py

--- pyday36.py
+++ pyday36.py
@@ -5,3 +5,4 @@ url =' https://curso-pthon-netlify.app/aula_03.html'
 navegador = Firefox()
 navegador.get(url) - # poderia ser :navegador.get( https://curso-pthon-netlify.app/aula_03.html)
 #so que como ja foi atribuído a variável url o site não é necessário
+a = nagador.find_element_by_tag_name('a')
