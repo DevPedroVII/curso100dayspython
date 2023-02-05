0d456b9cb35061d87a1d1b9b96a7f7ced6cdc65d
pedro Vinicius <singed161@gmail.com>
2023-02-04 22:20:05 -0300

    Update main.py

--- forkgame/main.py
+++ forkgame/main.py
@@ -18,4 +18,5 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 if guess in display:
         print(f"You've already guessed {guess}")
-
+for position in range(word_length):
+        letter = chosen_word[position]
