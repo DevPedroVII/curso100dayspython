e23147ab97d7e6c9cd1d951360e04ccec61b07e2
pedro Vinicius <singed161@gmail.com>
2023-02-04 22:22:12 -0300

    Update main.py

--- forkgame/main.py
+++ forkgame/main.py
@@ -22,4 +22,13 @@ for position in range(word_length):
         letter = chosen_word[position]
 if letter == guess:
             display[position] = letter
+if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+    lives -= 1
+    if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(f"{' '.join(display)}")
+
 
