084eed70958fef30e0e678af7c9f533c7fcefbfc
pedro Vinicius <singed161@gmail.com>
2023-02-05 21:35:06 -0300

    Update hangmanzs.py

--- forkgame/hangmanzs.py
+++ forkgame/hangmanzs.py
@@ -2,3 +2,6 @@ word_list = ["aardvark", "baboon", "camel"]
 import random
 chosen_word = random.choice(word_list)
 guess = input("Guess a letter: ").lower()
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
