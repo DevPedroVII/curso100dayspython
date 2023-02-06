c419788f2a1a4d33c9c4b18aa28d4c601c74dbd6
pedro Vinicius <singed161@gmail.com>
2023-02-06 20:22:10 -0300

    Update pyday38.py

--- pyday38.py
+++ pyday38.py
@@ -8,4 +8,4 @@ lista_n_ordenada = browser.find_element_by_tag_name('ul')
 
 Lis = lista_n_ordenada.browser.find_element_by_tag_name('li')
 
-Lis [0].find_element_by_tag_name('a').text #elemento 1 da lista
+Lis [0].find_element_by_tag_name('a').text
