2e4ef2810869cf7dea3f6998ca89de431dbf3e40
pedro Vinicius <singed161@gmail.com>
2023-02-06 20:19:40 -0300

    Update pyday38.py

--- pyday38.py
+++ pyday38.py
@@ -4,4 +4,4 @@ browser = Chrome ()
 
 browser.get('http://selenium.dunossauro.live/aula_04_a.html')
 
-lista_n_ordenada = browser.find_element_by_tag_name('ul')
+lista_n_ordenada = browser.find_element_by_tag_name('ul'
