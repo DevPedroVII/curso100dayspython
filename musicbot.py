18be488776b9daad14d8cd5e6a64e58b06696e4f
pedro Vinicius <singed161@gmail.com>
2023-02-10 21:42:09 -0300

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -1,3 +1,23 @@
 #day42
 
 
+from selenium import webdriver
+
+# Inicialize o driver do navegador
+driver = webdriver.Chrome()
+
+# Acesse o site de música
+driver.get("https://www.youtube.com/")
+
+# Localize a caixa de pesquisa no site
+search_box = driver.find_element_by_xpath('//input[@id="search"]')
+
+# Digite a música que você deseja reproduzir
+search_box.send_keys("music name")
+search_box.submit()
+
+# Localize o primeiro vídeo na lista de resultados de pesquisa
+video = driver.find_element_by_xpath('//a[@id="video-title"]')
+
+# Clique no vídeo para reproduzí-lo
+video.click()
