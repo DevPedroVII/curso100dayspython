a7c3aee7ec9fda4b56b8e18ec2e3e1cc7f1fce56
pedro Vinicius <singed161@gmail.com>
2023-02-13 19:44:35 -0300

    Update ideadeboot.py

---
+++ ideadeboot.py
@@ -0,0 +1,8 @@
+import tweepy
+import random
+
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
+
