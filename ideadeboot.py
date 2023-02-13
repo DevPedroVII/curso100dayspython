2574b4f5c3b4c6731f9421f42996c84f5d2c3327
pedro Vinicius <singed161@gmail.com>
2023-02-13 19:44:50 -0300

    Update ideadeboot.py

--- ideadeboot.py
+++ ideadeboot.py
@@ -6,3 +6,5 @@ consumer_secret = "your_consumer_secret"
 access_token = "your_access_token"
 access_token_secret = "your_access_token_secret"
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
