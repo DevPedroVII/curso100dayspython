a07bfedb8f03bebe0dc7dce81c880aaf9f8313b3
pedro Vinicius <singed161@gmail.com>
2023-02-18 22:05:21 -0300

    Create calculo.py

---
+++ calculo.py
@@ -0,0 +1,6 @@
+horas_trabalhadas = float(input("Digite o número de horas trabalhadas: "))
+valor_por_hora = float(input("Digite o valor por hora: "))
+
+salario_bruto = horas_trabalhadas * valor_por_hora
+
+print("O salário bruto é:", salario_bruto)
